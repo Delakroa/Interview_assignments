dfb9eec72d7d0298cacd6a381dc1c685b9da8558
delakroa <katon1986@gmail.com>
2021-12-02 10:11:41 +0300

    Изучение

--- Parsing_JSON.py
+++ Parsing_JSON.py
@@ -5,19 +5,50 @@ with open('test1.json') as file:
     data = json.load(file)
 for header in data['headers']:
     text = header['properties']
-    Id = text['Width']
-    print(Id)
+    Id = text['Id']
+    Name = text['Name']
+    Sid = text['Sid']
+    Enabled = text['Enabled']
+    X = text['X']
+    Y = text['Y']
+    Width = text['Width']
+    Height = text['Height']
+    # print(type(text))
+    print(Id, Name, Sid, Enabled, X, Y, Width, Height)
+
+# Создаём рабочую книгу
+book = openpyxl.Workbook()
+
+# Позиционирование на конкретном листе
+# в который мы осуществляем запись
+sheet = book.active  # по умолчанию это первый лист
+
+# Обращение к координатам ячейки
+sheet['A1'] = 'ID'
+sheet['B1'] = 'NAME'
+sheet['C1'] = 'SID'
+sheet['D1'] = 'ENABLED'
+sheet['E1'] = 'X'
+sheet['F1'] = 'Y'
+sheet['G1'] = 'WIDTH'
+sheet['H1'] = 'HEIGHT'
+
+row = 2
+for header in data['headers']:
+    text = header['properties']
+    # Обращение по координатам [ряд][колонка]
+    sheet[row][0].value = text['Id']
+    sheet[row][1].value = text['Name']
+    sheet[row][2].value = text['Sid']
+    sheet[row][3].value = text['Enabled']
+    sheet[row][4].value = text['X']
+    sheet[row][5].value = text['Y']
+    sheet[row][6].value = text['Width']
+    sheet[row][7].value = text['Height']
+    row += 1
 
 
-# print(data['headers'])
-# print(data['values'])
-
-# book = openpyxl.Workbook()
-#
-# sheet = book.active
-#
-# sheet['A2'] = 100
-# sheet['B3'] = 'hello'
-#
-# book.save("my_book.xlsx")
-# book.close()
+# Чтобы изменения вступили в силу
+# сохраняем книгу
+book.save("my_book.xlsx")
+book.close()
