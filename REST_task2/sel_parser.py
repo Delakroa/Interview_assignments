39a13d0117b1b7bec8917c23c52b42e73413cb90
delakroa <katon1986@gmail.com>
2021-12-04 22:52:11 +0300

    Добавил

--- REST_task2/sel_parser.py
+++ REST_task2/sel_parser.py
@@ -9,20 +9,25 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from bs4 import BeautifulSoup
 from time import sleep
-
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
+
 
 driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 url = 'https://service.nalog.ru/addrno.do'
 driver.get(url)
+
 sleep(3)
-input_tab = driver.find_element_by_class_name("no-data")
-input_tab.click()
+
+input_tab = driver.find_element_by_class_name("no-data").click()
+# input_tab.click()
+
 sleep(3)
-button = driver.find_element_by_css_selector('#txtFilter').click()
-print(button.text)
-button.send_keys('7840')
+
+
+button = driver.find_element_by_class_name('btn-node')
+# button.send_key('7840')
+button.click()
 
 
 
