e3f4c7616ae1209aadd02b73c0d98463fc6bdcc9
Delakroa <katon1986@gmail.com>
2022-06-22 11:04:48 +0300

    Создал класс Link

--- CKR_company/REST_task2/test/link.py
+++ CKR_company/REST_task2/test/link.py
@@ -1,7 +1,19 @@
 """Фаил содержит переменные и ссылки для тестов"""
 
-# Дирректория драйвера
-DRIVER_PATH = r'D:/Python library/Interview_assignments/CKR_company/REST_task2/chromedriver.exe'
 
-# Сайт с которым работаем
-FNS_RUSSIA = 'https://service.nalog.ru/addrno.do'
+class Link():
+    """Класс для ссылок"""
+
+    def __init__(self, driver_chrome, fns_russia) -> None:
+        """Инициализация атрибута класса"""
+        self.driver_chrome = driver_chrome
+        self.fns_russia = fns_russia
+
+    def driver_path(self):
+        """Дирректория драйвера"""
+        self.driver_chrome = r'D:/Python library/Interview_assignments/CKR_company/REST_task2/hromedriver.exe'
+        return self.driver_chrome
+
+    def get_site(self):
+        """Сайт с которым работаем"""
+        self.fns_russia = 'https://service.nalog.ru/addrno.do'
