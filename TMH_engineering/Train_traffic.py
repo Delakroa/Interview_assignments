0f3384baa34ce6df04cc054058d857a394c6c1d2
Delakroa <katon1986@gmail.com>
2022-07-20 14:00:28 +0300

    убрал __init__

--- TMH_engineering/Train_traffic.py
+++ TMH_engineering/Train_traffic.py
@@ -3,14 +3,12 @@
 class TrainSchedule:
     """Создание класса расписания поездов"""
 
-    def __init__(self):
-        """Инициализация атрибут"""
-
     def point_of_departure(self):
         """Установка пункта отправления"""
         while True:
             try:
-                point_departure = int(input("Введите номер пункта отправления (Один из четырёх) : "))
+                point_departure = int(
+                    input("Введите номер пункта отправления (Один из четырёх) : "))
                 if point_departure > 4 or point_departure <= 0:
                     print("Неверное значение пункта. Их всего четыре. \n")
 
@@ -26,7 +24,8 @@ class TrainSchedule:
             try:
                 print("Укажите время отправления поезда")
 
-                d_time_hour, d_time_minutes = map(int, input("Введите часы и минуты через пробел: ").split())
+                d_time_hour, d_time_minutes = map(int, input(
+                    "Введите часы и минуты через пробел: ").split())
 
                 if d_time_hour > 24 or d_time_hour < 0:
                     print("Некорректный ввод часов.\n")
@@ -35,7 +34,8 @@ class TrainSchedule:
                     print("Некорректный ввод минут.\n")
 
                 else:
-                    print(f"\nВремя отправления поезда {d_time_hour}ч. : {d_time_minutes}мин.\n")
+                    print(
+                        f"\nВремя отправления поезда {d_time_hour}ч. : {d_time_minutes}мин.\n")
                     return d_time_hour
 
             except ValueError:
@@ -47,7 +47,8 @@ class TrainSchedule:
             try:
                 print("Установите время прибытия поезда")
 
-                a_time_hour, a_time_minutes = map(int, input("Введите часы и минуты через пробел: ").split())
+                a_time_hour, a_time_minutes = map(int, input(
+                    "Введите часы и минуты через пробел: ").split())
 
                 if a_time_hour > 24 or a_time_hour < 0:
                     print("Некорректный ввод часов.\n")
@@ -61,7 +62,8 @@ class TrainSchedule:
                 #     print("Неверный ввод данных. Время прибытия не может быть меньше времени отправления!\n")
 
                 else:
-                    print(f"\nВремя прибытия поезда в {a_time_hour}ч. : {a_time_minutes}мин.\n")
+                    print(
+                        f"\nВремя прибытия поезда в {a_time_hour}ч. : {a_time_minutes}мин.\n")
 
                     return a_time_hour
 
@@ -73,7 +75,8 @@ class TrainSchedule:
         # Позже нужно будет реализовать логику( пункт отправления не может быть < пункта прибытия)
         while True:
             try:
-                point_arrival = int(input("Введите номер пункта прибытия (Один из четырёх) : "))
+                point_arrival = int(
+                    input("Введите номер пункта прибытия (Один из четырёх) : "))
                 if point_arrival > 4 or point_arrival <= 0:
                     print("Неверное значение пункта. Их всего четыре. \n")
 
@@ -94,7 +97,8 @@ class TrainSchedule:
         try:
             time = float(distance / speed)  # Время = расстояние / скорость
             if time <= 0:
-                print("Неверный вариант ввода данных: Дистанция не может быть нулевым значением")
+                print(
+                    "Неверный вариант ввода данных: Дистанция не может быть нулевым значением")
 
             elif distance == 1:
                 print(f"Время прибытия {time * 60} минуты")
@@ -103,7 +107,6 @@ class TrainSchedule:
                 return print(f"Расчётное время прибытие: {time} часа.")
         except ZeroDivisionError:
             print("Скорость не может быть нулевым значением!")
-
 
 
 ts = TrainSchedule()
